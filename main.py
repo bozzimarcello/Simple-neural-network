3df67eb58f7cd3d9aff8f71738124ca06faaec51
Marcello Bozzi <bozzimarcello@gmail.com>
2025-09-20 16:54:33 +0200

    Iris dataset added and cleaned

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import pandas as pd
 
 class Model(nn.Module):
   def __init__(self, in_features=4, h1=8, h2=9, out_features=3):
@@ -19,3 +21,12 @@ class Model(nn.Module):
 torch.manual_seed(41)
 
 model = Model()
+
+url = 'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv'
+my_df = pd.read_csv(url)
+
+my_df['species'] = my_df['species'].replace('setosa', 0.0)
+my_df['species'] = my_df['species'].replace('versicolor', 1.0)
+my_df['species'] = my_df['species'].replace('virginica', 2.0)
+
+print(my_df)
